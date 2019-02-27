209e94c250486b87d7fcabca61e3650ce5970139
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-02-27 06:25:27 -0800

    John Fortnite is Gone, the return of John Fornite

--- app.js
+++ app.js
@@ -50,5 +50,47 @@
 //     console.log('Hello aless!');
 // })
 
-let greet = require ('./greet.js');
-greet();
+// let greet = require ('./greet.js');
+// greet();
+
+function Person(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+//cualquier objeto que se crea a partir de esa clase persona van a tener acceso a ese prototipo
+Person.prototype.greet = function(){
+    console.log('Waddup, ' + this.firstname + ' ' + this.lastname);
+};
+
+let john = new Person ('John', 'Fornite');
+john.greet();
+
+
+// pass by value
+function change (b){
+    b = 2;
+}
+
+var a = 1;
+change (a);
+console.log(a);
+
+//pass by reference
+function changeObj(d){
+    d.prop1 = function(){};
+    d.prop2 = {};
+}
+
+let c = {};
+c.prop1 = {};
+changeObj(c);
+console.log(c);
+
+//inmediatly invoked function expression
+let firstname = 'Jane';
+
+(function(){
+    let firstname = 'John';
+    console.log(firstname)
+}());
+console.log(firstname);
