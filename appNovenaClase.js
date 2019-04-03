e107132714e71fcbc3591d65ce573e65cfc944f7
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-04-03 07:49:24 -0600

    Thanos will return

--- appNovenaClase.js
+++ appNovenaClase.js
@@ -1,8 +1,12 @@
 let http = require('http');
-
+let fs =require('fs');
 http.createServer(function (req, res) {
 
-    res.writeHead(200, { 'Content-Type' : 'text/plain' });
+    res.writeHead(200, { 'Content-Type' : 'text/html' });
+    let html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    let message = 'Quieres hospitalidad, ahi hay una lata de chinga tu madre, sirvete';
+    html = html.replace('{Message}', message);
+    res.end(html);
     res.end('Hello world\n');
     
 }).listen(1337, 'localhost');
