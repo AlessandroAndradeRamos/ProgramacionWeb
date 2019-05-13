9f6bdb0d90df190869e3122b35c14430261c5e91
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-05-13 07:16:03 -0500

    John Fornite's ManNest

--- appDecimoTerceraClase.js
+++ appDecimoTerceraClase.js
@@ -1,7 +1,36 @@
+
 let request = require('request');
+let fahrenheitToCelsius = require('fahrenheit-to-celsius');
 
-
-request('https://api.darksky.net/forecast/7861d0f3196d70329299ace94b9e4966/19.2433,-103.725', (err, respuesta, body)=>{
-    let data = JSON.parse(body);
-    console.log(data['currently']['temperature']);
+request('https://us1.locationiq.com/v1/search.php?key=44fcebb8c5c660&q=Colima&format=json', function(err, res, body){
+    if(err)
+    console.log(err);
+    else {
+        let data = JSON.parse(body);
+        let latitud = data[0]['lat'];
+        let longitud = data[0]['lon']
+        console.log(`latitud:${latitud}, longitud:${longitud}`);
+        request(`https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/${latitud},${longitud}`, function(err, res, body){
+            if(err)
+            console.log(err);
+            else 
+            {
+                let data = JSON.parse(body);
+                let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+                console.log(temperature + '°C');                 
+            }
+        })
+        let options = { method: 'GET',
+        url: 'https://api.openuv.io/api/v1/uv',
+        qs: { lat: `${latitud}`, lng: `${longitud}`},
+        headers: 
+         { 'content-type': 'application/json',
+           'x-access-token': 'ee6fef521196eaa60d6e79743993c111' } };
+       
+       request(options, function (error, response, body) {
+         if (error) throw new Error(error);
+         let data = JSON.parse(body);
+         console.log(data['result']['uv'] + '°');
+       });
+    }
 });
