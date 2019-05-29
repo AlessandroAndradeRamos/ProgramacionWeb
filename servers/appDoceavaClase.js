09153be7d6864a545b5194c3182f9ada129df136
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-05-29 08:42:19 -0500

    John Fornite is undefined

--- servers/appDoceavaClase.js
+++ servers/appDoceavaClase.js
@@ -1,13 +1,34 @@
 let express = require('express');
 let app = express();
+const mongoose = require('mongoose');
 let port = process.env.PORT || 3000;
 //CURLY BRACES MY MY MY
+
+
+mongoose.connect('mongodb://root:root123@ds147125.mlab.com:47125/addressbookgac');
+let Schema = mongoose.Schema;
+
+let personSchema = new Schema({
+firstname: String
+// lastName: String,
+// address: String
+});
+let Person = mongoose.model('Person', personSchema);
 app.get('/', function (req, res) {
     res.send('<html><head></head><body><h1>HeWWo JOhn Fornite</h1></body></html>');
 });
 
 app.get('/person/:id', function (req, res) {
+
+let johnfortnite = Person({
+    firstname: `${req.param.id}`
+    // lastName: 'Fornite',
+    // address: '420-69 Street'
+});
+johnfortnite.save(function (err) {
+    if(err) throw err;
     res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
 })
 
 app.get('/api', function (req, res) {
